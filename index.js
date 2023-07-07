41e28d083ed8e36482fa2be2917472c18ec0b32a
0x464e <36742501+0x464e@users.noreply.github.com>
2023-07-07 18:38:47 +0300

    Bump discord.js to dev version to support new display names

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { Intents, Client, Permissions } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, PermissionsBitField } = require('discord.js');
 const cron = require('node-cron');
 const client = new Client(
     {
-        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
+        intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
         //partials needed to detect users even if they're appearing offline
-        partials: ['USER', 'GUILD_MEMBER']
+        partials: [Partials.User, Partials.GuildMember]
     });
 const { TOKEN, GUILDID, USERID } = require('./config.json');
 let last_avi;
@@ -20,12 +20,12 @@ client.on('userUpdate', (old_user, new_user) => {
                 change_avi(new_user.displayAvatarURL()).catch(error => { console.log(error); last_avi = null });
             }).catch(console.error)
         ).catch(console.error);
-    else if (old_user.username !== new_user.username) //user changed their username
+    else if (old_user.globalName !== new_user.globalName) //user changed their username
     {
        client.guilds.fetch(GUILDID).then(guild =>
            guild.members.fetch(USERID).then(user => {
                if (user.nickname == null) //if the user has a nickname, we don't care about a username change
-                   guild.me.setNickname(new_user.username).catch(console.error);
+                   guild.members.me.setNickname(new_user.globalName).catch(console.error);
            }).catch(console.error)
        ).catch(console.error);
     }
@@ -36,11 +36,11 @@ client.on('guildMemberUpdate', async (old_user, new_user) => {
         return;
 
     if (new_user.id === USERID && old_user.nickname !== new_user.nickname) //user changed their nickname
-        new_user.guild.me.setNickname(new_user.nickname ?? new_user.user.username).catch(console.error);
+        new_user.guild.members.me.setNickname(new_user.nickname ?? new_user.user.globalName).catch(console.error);
     else if (new_user.id === USERID && old_user.avatar !== new_user.avatar) //user changed their guild specific avatar
        change_avi(new_user.avatarURL() ?? new_user.displayAvatarURL()).catch(error => { console.log(error); last_avi = null });
     else if (new_user.id === client.user.id && old_user.nickname !== new_user.nickname) //the bot's nickname was changed by someone
-        new_user.guild.members.fetch(USERID).then(user => new_user.guild.me.setNickname(user.nickname ?? user.user.username).catch(console.error));
+        new_user.guild.members.fetch(USERID).then(user => new_user.guild.members.me.setNickname(user.nickname ?? user.user.globalName).catch(console.error));
 });
 
 let change_avi = (avi_url) => client.user.setAvatar((last_avi = avi_url).replace(/(?:.(?!\.))+$/, '.png?size=4096'));
@@ -50,13 +50,13 @@ client.login(TOKEN).then(() =>
     console.log('Connected as ' + client.user.tag);
     client.guilds.fetch(GUILDID).then(guild => {
         guild.members.fetch(USERID).then(user => {
-            if (!guild.me.permissions.has(Permissions.FLAGS.CHANGE_NICKNAME))
+            if (!guild.members.me.permissions.has(PermissionsBitField.Flags.ChangeNickname))
             {
                 console.log('No permissions to change nickname, exiting');
                 process.exit();
             }
             change_avi(user.avatarURL() ?? user.displayAvatarURL()).catch(error => { console.log(error); last_avi = null });
-            guild.me.setNickname(user.nickname ?? user.user.username).catch(console.error);
+            guild.members.me.setNickname(user.nickname ?? user.user.globalName).catch(console.error);
         }).catch(console.error);
     }).catch(console.error);
 }).catch(console.error);
@@ -72,8 +72,8 @@ cron.schedule('0 * * * *', () => {
             let avi_url = user.avatarURL() ?? user.displayAvatarURL();
             if (last_avi !== avi_url)
                 change_avi(avi_url).catch(error => { console.log(error); last_avi = null });
-            if (guild.me.nickname !== user.nickname ?? user.user.username)
-                guild.me.setNickname(user.nickname ?? user.user.username).catch(console.error);
+            if (guild.members.me.nickname !== user.nickname ?? user.user.globalName)
+                guild.members.me.setNickname(user.nickname ?? user.user.globalName).catch(console.error);
         }).catch(console.error);
     }).catch(console.error);
 });
